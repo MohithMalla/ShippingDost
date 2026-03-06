7f1845c5032a0a95ec914676afaf1054a7053bab
Malla MohithSai <167329211+MohithMalla@users.noreply.github.com>
2026-03-06 16:49:23 +0530

    Update allowed origins in CORS configuration

--- ShippingDost-backend/src/main/java/com/shippingdost/shipping/config/SecurityConfig.java
+++ ShippingDost-backend/src/main/java/com/shippingdost/shipping/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 CorsConfiguration config = new CorsConfiguration();
                 config.setAllowedOrigins(List.of(
                         "http://localhost:5173", 
-                        "https://shipping-dost.vercel.app" 
+                        "https://shipping-dost.netlify.app/" 
                 ));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
                 config.setAllowedHeaders(List.of("*"));
